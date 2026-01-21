cb1144868dbd4abb027ce23234ee112956da21b8
tuanminhongthanlan <tuannminhh496@gmail.com>
2026-01-21 20:43:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ noBtn.onmouseover = function () {
     noBtn.style.left = Math.random() * window.innerWidth + "px";
     noBtn.style.top = Math.random() * window.innerHeight + "px";
 };
+.heart {
+    position: fixed;
+    bottom: 0;
+    font-size: 20px;
+    animation: fly 4s linear forwards;
+    color: red;
+}
+
+@keyframes fly {
+    0% {
+        transform: translateY(0) scale(1);
+        opacity: 1;
+    }
+    100% {
+        transform: translateY(-100vh) scale(2);
+        opacity: 0;
+    }
+}
